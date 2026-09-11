fc4000afd70ab37b53baba9741ca51c80c835d59
Caio Trindade <caiomedtrindade@gmail.com>
2026-09-10 23:11:34 -0300

    feat: add golang and proto

---
+++ services/catalogo/cmd/server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+// O servidor gRPC de verdade entra quando o contrato em
+// protos/catalogo.proto for gerado
+func main() {
+	log.Println("catalogo: esqueleto ativo (gRPC ainda não implementado)")
+}
