4fa4df8f0d7efd5f0fc0cef1f75c2c5c5acf6f07
Caio Trindade <caiomedtrindade@gmail.com>
2026-09-20 18:14:25 -0300

    feat: adiciona novas dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,15 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.navigation.compose)
+            implementation(libs.material3.window.size)
+            implementation(libs.material3.adaptive)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
         }
 
         androidMain.dependencies {
@@ -45,6 +54,12 @@ kotlin {
         }
 
         val desktopMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+
+        val desktopTest by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
             }
